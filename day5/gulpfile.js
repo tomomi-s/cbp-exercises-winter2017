ccd0f581b029e197279edb6e39a7a97f5112b2c0
Unknown <tomomi.suda03@gmail.com>
2017-01-19 09:42:35 +0100

    commit day7 and day8

--- day5/gulpfile.js
+++ day5/gulpfile.js
@@ -3,9 +3,12 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     browserSync.init({
-        server: {
-            baseDir: './'
+        proxy: {
+		target: “http://cbp-exercises.local/”,
         },
+	open: ‘external’,
+	host: ‘cbp-exercises.local’
+	port: 3000,
         files: ['*.css', '*.html']
     });
 });
---
+++ day8/js/script.js
@@ -0,0 +1,37 @@
+var value = null; type = null;
+
+// value = '123'; //assign '123' into variable 'value'
+// type = typeof value; //we get the type of variable 'value' and assignit into cariable 'type'
+// console.log(type); // we output the variable 'type' (containg the type of variable 'value')
+
+// value = '123';
+// type = typeof value;
+// console.log(type);
+
+// value = '123';
+// type = typeof value;
+// console.log(type);
+
+// value = '123';
+// type = typeof value;
+// console.log(type);
+
+// value = document ;
+// type = typeof value;
+// console.log(type);
+
+// value = { goo: 'bar' };
+// type = typeof value;
+// console.log(type);
+
+var message_element = document.getElementById('message'); 
+console.log(message_element);
+console.log(typeof message_element);
+// 123
+// '123 Times Square'
+// 12.3
+// true
+// 'true'
+// [1, 2, 3]
+// { foo: 'bar' }
+// document
